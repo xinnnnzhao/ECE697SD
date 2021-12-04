17f7b7139f3137b03839282206b23d4200f4a58e
xinnnnzhao <xinnnnzhao@gmail.com>
2021-12-04 15:48:21 -0500

    zip

--- DepthCameraSide/upload.ps1
+++ DepthCameraSide/upload.ps1
@@ -1,3 +1,4 @@
 while(1){
-scp -i ../FTPServer/ece-697-aws-key.pem $args ec2-user@ec2-34-235-116-199.compute-1.amazonaws.com:~/data/
+Compress-Archive -Path $args".ply" -DestinationPath $args".zip"
+scp -i ../FTPServer/ece-697-aws-key.pem $args".zip" ec2-user@ec2-34-235-116-199.compute-1.amazonaws.com:~/data/
 }
