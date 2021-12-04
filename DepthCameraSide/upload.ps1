858b54734fccdb4b55c182a4c275996eaf814764
xinnnnzhao <xinnnnzhao@gmail.com>
2021-12-04 17:05:02 -0500

    xyz

--- DepthCameraSide/upload.ps1
+++ DepthCameraSide/upload.ps1
@@ -1,5 +1,5 @@
 while(1){
 rm $args".zip"
-Compress-Archive -Path $args".ply" -DestinationPath $args".zip"
+Compress-Archive -Path $args".xyz" -DestinationPath $args".zip"
 scp -i ece-697-aws-key.pem $args".zip" ec2-user@ec2-34-235-116-199.compute-1.amazonaws.com:~/data/
 }
--- VRGlassSide/dowload.scp.ps1
+++ VRGlassSide/dowload.scp.ps1
@@ -1,7 +1,7 @@
 while(1){
 scp -i ece-697-aws-key.pem ec2-user@ec2-34-235-116-199.compute-1.amazonaws.com:~/data/out.zip  out.zip
-rm .\temp\out.ply
+rm .\temp\out.xyz
 Expand-Archive -Path out.zip -DestinationPath temp
-rm out.ply
-cp temp/out.ply ./
+rm out.xyz
+cp temp/out.xyz ./
 }
