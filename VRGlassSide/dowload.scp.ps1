ffc26cdf445e4a5458937d01086e91ed7fe15ff0
xinnnnzhao <xinnnnzhao@gmail.com>
2021-12-04 16:20:38 -0500

    dwd

--- VRGlassSide/dowload.scp.ps1
+++ VRGlassSide/dowload.scp.ps1
@@ -1,7 +1,7 @@
 while(1){
 scp -i ../FTPServer/ece-697-aws-key.pem ec2-user@ec2-34-235-116-199.compute-1.amazonaws.com:~/data/$args".zip"  $args".zip"
-rm $args".ply"
-rm $args"1.ply"
-Expand-Archive -Path $args".zip" -DestinationPath $args"1.ply"
-mv $args"1.ply" $args".ply"
+rm temp/out.ply
+Expand-Archive -Path $args".zip" -DestinationPath temp
+rm out.ply
+mv temp/out.ply ./
 }
