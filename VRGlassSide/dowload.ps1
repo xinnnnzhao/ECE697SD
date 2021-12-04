795ad82567086dc22060000d3dec76d7847c0b06
xinnnnzhao <xinnnnzhao@gmail.com>
2021-12-04 14:57:17 -0500

    in real time

--- VRGlassSide/dowload.ps1
+++ VRGlassSide/dowload.ps1
@@ -1,1 +1,3 @@
- wget -Uri "http://ec2-34-235-116-199.compute-1.amazonaws.com:8000/$args" -OutFile $args[0]
+while(1){
+wget -Uri "http://ec2-34-235-116-199.compute-1.amazonaws.com:8000/$args" -OutFile $args[0]
+}
--- VRGlassSide/dowload.scp.ps1
+++ VRGlassSide/dowload.scp.ps1
@@ -1,1 +1,3 @@
+while(1){
 scp -i ../FTPServer/ece-697-aws-key.pem ec2-user@ec2-34-235-116-199.compute-1.amazonaws.com:~/data/$args  $args
+}
