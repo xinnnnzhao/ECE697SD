78967af787fdc30eee5137b8e6fe5ef09976aaf3
xinnnnzhao <xinnnnzhao@gmail.com>
2021-12-04 15:44:26 -0500

    rm

--- VRGlassSide/dowload.scp.ps1
+++ VRGlassSide/dowload.scp.ps1
@@ -1,4 +1,5 @@
 while(1){
 scp -i ../FTPServer/ece-697-aws-key.pem ec2-user@ec2-34-235-116-199.compute-1.amazonaws.com:~/data/$args".ply"  $args"1.ply"
+rm $args".ply"
 mv $args"1.ply" $args".ply"
 }
