f841aa2eafe694cbe6ba9e9eba395303bb451a8b
XiaohaoXia <xiaxiaohao1996@qq.com>
2021-12-23 12:50:03 +0800

    code of script

---
+++ Code of Unity/dowload.scp.ps1
@@ -0,0 +1,10 @@
+while(1){
+scp -i ece-697-aws-key.pem ec2-user@ec2-34-235-116-199.compute-1.amazonaws.com:~/data/out.zip  out.zip
+if(Test-Path -Path ".\temp\out.xyz" -PathType Leaf){
+	rm .\temp\out.xyz
+}
+
+Expand-Archive -Path out.zip -DestinationPath temp
+rm out.xyz
+mv temp/out.xyz ./
+}
